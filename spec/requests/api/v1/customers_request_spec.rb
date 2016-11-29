1e08fbe022b576dd87336364fb7bd30199ba883f
Ben Pepper <bfpepper87@gmail.com>
2016-11-29 11:53:17 -0700

    add find functuon for everything but created_at and updated_at.

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -25,4 +25,49 @@ describe "customers endpoints" do
       expect(customers["first_name"]).to eq("Danny")
     end
   end
+
+  context "GET /custoemrs/find" do
+    let (:customers) { create(:customer) }
+
+    it "finds a customer by id" do
+      get "/api/v1/customers/find?id=#{customers.id}"
+
+      customer = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(customer["id"]).to eq(("#{customers.id}").to_i)
+    end
+    it "finds a customer by first_name" do
+      get "/api/v1/customers/find?first_name=#{customers.first_name}"
+
+      customer = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(customer["first_name"]).to eq("#{customers.first_name}")
+    end
+    it "finds a customer by last_name" do
+      get "/api/v1/customers/find?last_name=#{customers.last_name}"
+
+      customer = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(customer["last_name"]).to eq("#{customers.last_name}")
+    end
+    # it "finds a customer by created_at" do
+    #   get "/api/v1/customers/find?created_at=#{customers.created_at}"
+    #
+    #   customer = JSON.parse(response.body)
+    #
+    #   expect(response).to be_success
+    #   expect(customer["created_at"]).to eq(("#{customers.created_at}").to_i)
+    # end
+    # it "finds a customer by updated_at" do
+    #   get "/api/v1/customers/find?updated_at=#{customers.updated_at}"
+    #
+    #   customer = JSON.parse(response.body)
+    #
+    #   expect(response).to be_success
+    #   expect(customer["updated_at"]).to eq(("#{customers.updated_at}").to_i)
+    # end
+  end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -26,4 +26,65 @@ describe "Items endpoint" do
       expect(item_show["name"]).to eq(item1.name)
     end
   end
+
+  context "GET /items/find" do
+    let (:items) { create(:item) }
+
+    it "finds a item by id" do
+      get "/api/v1/items/find?id=#{items.id}"
+
+      item = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(item["id"]).to eq(("#{items.id}").to_i)
+    end
+    it "finds a item by name" do
+      get "/api/v1/items/find?name=#{items.name}"
+
+      item = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(item["name"]).to eq("#{items.name}")
+    end
+    it "finds a item by description" do
+      get "/api/v1/items/find?description=#{items.description}"
+
+      item = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(item["description"]).to eq("#{items.description}")
+    end
+    it "finds a item by merchant_id" do
+      get "/api/v1/items/find?merchant_id=#{items.merchant_id}"
+
+      item = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(item["merchant_id"]).to eq(("#{items.merchant_id}").to_i)
+    end
+    it "finds a item by unit_price" do
+      get "/api/v1/items/find?unit_price=#{items.unit_price}"
+
+      item = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(item["unit_price"]).to eq(("#{items.unit_price}").to_i)
+    end
+    # it "finds a item by created_at" do
+    #   get "/api/v1/items/find?created_at=#{items.created_at}"
+    #
+    #   item = JSON.parse(response.body)
+    #
+    #   expect(response).to be_success
+    #   expect(item["created_at"]).to eq(("#{items.created_at}").to_i)
+    # end
+    # it "finds a item by updated_at" do
+    #   get "/api/v1/items/find?updated_at=#{items.updated_at}"
+    #
+    #   item = JSON.parse(response.body)
+    #
+    #   expect(response).to be_success
+    #   expect(item["updated_at"]).to eq(("#{items.updated_at}").to_i)
+    # end
+  end
 end
