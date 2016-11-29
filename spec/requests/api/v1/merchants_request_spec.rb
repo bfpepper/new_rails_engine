250a71f9f8cdf0cea81d229025e85694ab968958
Michael Scherer <michael.c.scherer@gmail.com>
2016-11-28 18:35:17 -0700

    Add merchants controller and endpoints

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -4,7 +4,13 @@ describe "merchants endpoints" do
   context "GET /merchants" do
     it "returns a list of all merchants" do
       create_list(:merchant, 2)
-      
+
+      get "/api/v1/merchants.json"
+
+      merchants = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(merchants.count).to eq(2)
     end
   end
 end
