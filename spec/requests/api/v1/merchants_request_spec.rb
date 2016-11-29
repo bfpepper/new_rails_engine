ee273263424490f62ce156744bbcebac55efe537
Michael Scherer <michael.c.scherer@gmail.com>
2016-11-28 18:46:09 -0700

    Add show merchant endpoint

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -13,4 +13,16 @@ describe "merchants endpoints" do
       expect(merchants.count).to eq(2)
     end
   end
+  context "GET /merchant" do
+    it "returns one merchant" do
+      merchant = create(:merchant, name: "Merchy Merch")
+
+      get "/api/v1/merchants/#{merchant.id}.json"
+
+      merchants = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(merchant["name"]).to eq("Merchy Merch")
+    end
+  end
 end
