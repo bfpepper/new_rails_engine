32d2add8100966f0729cd3f7ede5ec9cfb520cc0
Ben Pepper <bfpepper87@gmail.com>
2016-11-30 11:20:44 -0700

    find transactions by a given parameter.

--- spec/requests/api/v1/transactions/find_all_spec.rb
+++ spec/requests/api/v1/transactions/find_all_spec.rb
@@ -12,6 +12,39 @@ context "Finds all records by a given parameter" do
     expect(response).to be_success
     expect(trans_list.count).to eq(3)
   end
+
+  it "finds all records for a given CC number" do
+    create_list(:transaction, 3, credit_card_number: 456)
+
+    get "/api/v1/transactions/find_all?credit_card_number=456"
+
+    trans_list = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(trans_list.count).to eq(3)
+  end
+
+  it "finds all records for a given cc exp date" do
+    create_list(:transaction, 3, credit_card_expiration_date: "11/7")
+
+    get "/api/v1/transactions/find_all?credit_card_expiration_date=11/7"
+
+    trans_list = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(trans_list.count).to eq(3)
+  end
+
+  it "finds all records for a given result" do
+    create_list(:transaction, 3, result: "success")
+
+    get "/api/v1/transactions/find_all?status=success"
+
+    trans_list = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(trans_list.count).to eq(3)
+  end
 end
 
 # "invoice_id"
