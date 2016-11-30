f161b2ebb35aa254b0dd1193054381a6b69b71ff
Michael Scherer <michael.c.scherer@gmail.com>
2016-11-29 17:06:39 -0700

    Merchant Revenue BI query refactor"

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
   def show
-    render json: Merchant.find(params[:merchant_id]).invoices.joins(:invoice_items).sum("quantity * unit_price")
+    render json: Merchant.find(params[:merchant_id])
+      .invoices
+      .joins(:transactions)
+      .where.not(transactions: {result: "failed"})
+      .joins(:invoice_items)
+      .sum("quantity * unit_price")
   end
 
 end
--- spec/requests/api/v1/merchants/revenue_request_spec.rb
+++ spec/requests/api/v1/merchants/revenue_request_spec.rb
@@ -4,8 +4,13 @@ describe "Merchant Revenue" do
   context "GET /merchant/:id/revenue" do
     it "can get the revenue for a merchant" do
       merchant = create(:merchant, name: "Merchy Merch")
-      invoice = create(:invoice, merchant: merchant)
-      create_list(:invoice_item, 2, invoice: invoice, quantity: 2, unit_price: 2)
+      invoice1 = create(:invoice, merchant: merchant)
+      transaction1 = create(:transaction, invoice: invoice1)
+      invoice2 = create(:invoice, merchant: merchant)
+      transaction2 = create(:transaction, result: "failed", invoice: invoice2)
+      invoice_item1 = create(:invoice_item, invoice: invoice1, quantity: 2, unit_price: 2)
+      invoice_item2 = create(:invoice_item, invoice: invoice1, quantity: 2, unit_price: 2)
+      invoice_item3 = create(:invoice_item, invoice: invoice2, quantity: 2, unit_price: 2)
 
       get "/api/v1/merchants/#{merchant.id}/revenue"
 
